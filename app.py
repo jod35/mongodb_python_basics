419da258695dd6b43ddbac7dabd35703cfc1796f
jod35 <jodestrevin@gmail.com>
2019-10-30 10:55:02 -0700

    created a collection in the db

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ db=client['mydb']
 # print(db)
 # Database(MongoClient(host=['localhost:27017'], document_class=dict, tz_aware=False, connect=True), 'mydb')
 
+#creating a collection in the database
+dogs=db.dogs
+
+print?(dogs)
 
 
 #
