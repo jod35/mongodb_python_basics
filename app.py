29d9382e2b0b39cd6c40f51065db72cd53cd514f
jod35 <jodestrevin@gmail.com>
2019-10-30 10:54:37 -0700

    created the database

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from pymongo import MongoClient
 
-client =MongoClient()
-print (client)
+#connecting to mongodb on localhost:27017
+client =MongoClient('localhost',27017)
+#print (client)
 # MongoClient(host=['localhost:27017'], document_class=dict, tz_aware=False, connect=True)
+
+#creating a database
+db=client['mydb']
+# print(db)
+# Database(MongoClient(host=['localhost:27017'], document_class=dict, tz_aware=False, connect=True), 'mydb')
+
+
+
+#
