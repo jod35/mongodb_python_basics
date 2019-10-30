86ec22c5162f8074121baa51c55524d6b8e070be
jod35 <jodestrvin@gmail.com>
2019-10-30 12:22:09 -0700

    started a simple console app for products

--- product_app.py
+++ product_app.py
@@ -8,4 +8,32 @@ db=client['shop']
 products=db.products
 print(products)
 
-name=input("enter")
+print("Welcome to the product App!")
+name=input("enter enter product name: ")
+stock=input(("enter enter product stock: "))
+cost_price=int(input("enter enter product cost_price: "))
+markup=int(input("enter enter product markup: "))
+discount=int(input("enter enter product discount: "))
+
+disc= discount/100
+makup=cost_price*(markup/100)
+sell_price=cost_price+makup
+gross_price=sell_price-disc
+
+product_info= {}
+product_info['name']=name
+product_info['cost_price']=cost_price
+product_info['stock']=stock
+product_info['cost_price']=cost_price
+product_info['markup']=markup
+product_info['selling_price']=gross_price
+
+print(product_info)
+
+products_list=[]
+
+res=products.insert_one(product_info)
+
+if res.acknowledged:
+    print("New document with ID %s has been created successfully"%res.inserted_id)
+
