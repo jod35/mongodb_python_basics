a6b50e456d2cee89ee805a4824639fe814c39bae
jod35 <jodestrevin@gmail.com>
2019-10-30 11:16:06 -0700

    added many records

--- app.py
+++ app.py
@@ -18,14 +18,54 @@ dogs=db.dogs
 
 #creating a docment
 
-dog ={
-    'name':"Doggy",
-    'age':3,
-    'specie':'German Shepherd',
-    'owner':'jona'
-}
+# dog ={
+#     'name':"Doggy",
+#     'age':3,
+#     'specie':'German Shepherd',
+#     'owner':'jona'
+# }
 
-#insert the document in the database
-result=dogs.insert_one(dog)
+# #insert the document in the database
+# result=dogs.insert_one(dog)
 
-print(result)
+# if result.acknowledged:
+#     print('Course Added successfully and its ID is ',result.inserted_id)
+#     # Course Added successfully and its ID is  5db9d1149805b9ac8f609304
+
+
+# inserting many documents
+dogs_array=[
+    {
+        'name':'Bob',
+        'specie':'German Shepherd',
+        'age':2,
+        'owner':'Jannet'
+    },
+     {
+        'name':'Bob',
+        'specie':'German Shepherd',
+        'age':7,
+        'owner':'Walter'
+    }, {
+        'name':'Bob',
+        'specie':'Geared',
+        'age':6,
+        'owner':'Walter'
+    }, {
+        'name':'Bobby',
+        'specie':'St Benard',
+        'age':5,
+        'owner':'James'
+    },
+     {
+        'name':'Babbie',
+        'specie':'Rotweiler',
+        'age':5,
+        'owner':'Jeremiah'
+    }
+]
+
+result1=dogs.insert_many(dogs_array)
+print(result1)
+
+# <pymongo.results.InsertManyResult object at 0x03046738>
